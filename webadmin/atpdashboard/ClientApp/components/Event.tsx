a3ade39e75bd94f2781a6f530cd6b55685c4c6fb
andy powell <apuk45@gmail.com>
2018-05-31 13:39:27 -0500

    adding in react core app to display info from api

---
+++ webadmin/atpdashboard/ClientApp/components/Event.tsx
@@ -0,0 +1,65 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface EventState {
+    events: Events[];
+    loading: boolean;
+}
+
+export class Event extends React.Component<RouteComponentProps<{}>, EventState> {
+    constructor() {
+        super();
+        this.state = { events: [], loading: true };
+
+        fetch('http://localhost:5000/api/events/keyword=mean%20girls')
+            .then(response => response.json() as Promise<Events[]>)
+            .then(data => {
+                //console.log('data obj = ' + data);
+                this.setState({ events: data, loading: false });
+            });
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Event.renderEventsTable(this.state.events);
+
+        return <div>
+            <h1>Events</h1>
+            <p>This component demonstrates fetching data from the server.</p>
+            {contents}
+        </div>;
+    }
+
+    private static renderEventsTable(events: Events[]) {
+        return <table className='table'>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>type</th>
+                    <th>name</th>
+                    <th>url</th>
+                </tr>
+            </thead>
+            <tbody>
+                {events.map(event =>
+                    <tr key={event.id}>
+                        <td>{event.id}</td>
+                        <td>{event.type}</td>
+                        <td>{event.name}</td>
+                        <td>{event.url}</td>
+                        
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+    }
+}
+
+interface Events {
+    id: string;
+    name: string;
+    type: string;
+    url: string;
+}
